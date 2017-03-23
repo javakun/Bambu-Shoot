930d4aeb211dbca937ff5398821d7bdd6170fcce
javier colon <javier.colon15@upr.edu>
2017-03-23 12:44:23 -0400

    Working on Android GUI

--- BambuShootProject/BambuShootProject.Droid/MainActivity.cs
+++ BambuShootProject/BambuShootProject.Droid/MainActivity.cs
@@ -21,13 +21,7 @@ namespace BambuShootProject.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+		
 		}
 	}
 }
--- BambuShootProject/BambuShootProject.Droid/Resources/Resource.designer.cs
+++ BambuShootProject/BambuShootProject.Droid/Resources/Resource.designer.cs
@@ -61,7 +61,22 @@ namespace BambuShootProject.Droid
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int linearLayout2 = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView3 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView4 = 2131034117;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView5 = 2131034114;
 			
 			static Id()
 			{
