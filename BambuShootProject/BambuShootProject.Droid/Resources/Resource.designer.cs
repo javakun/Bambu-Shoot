75df39568da6b5f5118dfa5270b1fa51b51bd766
javier colon <javier.colon15@upr.edu>
2017-03-23 13:47:56 -0400

    no message

--- BambuShootProject/BambuShootProject.Droid/Resources/Resource.designer.cs
+++ BambuShootProject/BambuShootProject.Droid/Resources/Resource.designer.cs
@@ -45,7 +45,10 @@ namespace BambuShootProject.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int BambuShootIcon = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
@@ -61,7 +64,7 @@ namespace BambuShootProject.Droid
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int imageView1 = 2131034112;
+			public const int BambuShootIcon = 2131034112;
 			
 			static Id()
 			{
