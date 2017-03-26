1a0f1333321cbdedf20107f99389b3345d48ac39
javier colon <javier.colon15@upr.edu>
2017-03-25 23:45:04 -0400

    Created Instruction Dialog

--- BambuShootProject/BambuShootProject.Droid/MainActivity.cs
+++ BambuShootProject/BambuShootProject.Droid/MainActivity.cs
@@ -12,7 +12,8 @@ namespace BambuShootProject.Droid
 	[Activity (Label = "Bambú Shoot App", MainLauncher = true, Icon = "@drawable/BambuShootIcon")]
 	public class MainActivity : Activity
 	{
-     
+
+        private Button gBtnInstructions;            // Initialize Button as Global Variable
 
         protected override void OnCreate (Bundle bundle)
 		{
@@ -20,9 +21,18 @@ namespace BambuShootProject.Droid
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
-           
 
+            gBtnInstructions = FindViewById<Button>(Resource.Id.instructionsbtn);   //Link Button variable with Axml button
+            gBtnInstructions.Click += gBtnInstructions_Click;
 
+        }
+
+        private void gBtnInstructions_Click(object sender, EventArgs e)
+        {
+            //Pull the Instruction Dialog
+            FragmentTransaction transaction = FragmentManager.BeginTransaction();  // Pull up the dialog from the activity
+            Instructions_Dialog InstrucDialog = new Instructions_Dialog();
+            InstrucDialog.Show(transaction,"dialog fragment");
         }
     }
 }
--- BambuShootProject/BambuShootProject.Droid/Resources/Resource.designer.cs
+++ BambuShootProject/BambuShootProject.Droid/Resources/Resource.designer.cs
@@ -28,6 +28,25 @@ namespace BambuShootProject.Droid
 		{
 		}
 		
+		public partial class Animation
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int slide_right = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int slide_up = 2130968577;
+			
+			static Animation()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Animation()
+			{
+			}
+		}
+		
 		public partial class Attribute
 		{
 			
@@ -81,20 +100,23 @@ namespace BambuShootProject.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int BambuShootLogo = 2131034112;
+			// aapt resource value: 0x7f070001
+			public const int BambuShootLogo = 2131165185;
 			
-			// aapt resource value: 0x7f050004
-			public const int databasebtn = 2131034116;
+			// aapt resource value: 0x7f070000
+			public const int Instructions = 2131165184;
 			
-			// aapt resource value: 0x7f050003
-			public const int mydatareportlibrarybtn = 2131034115;
+			// aapt resource value: 0x7f070005
+			public const int databasebtn = 2131165189;
 			
-			// aapt resource value: 0x7f050002
-			public const int processnewimagebtn = 2131034114;
+			// aapt resource value: 0x7f070002
+			public const int instructionsbtn = 2131165186;
 			
-			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			// aapt resource value: 0x7f070004
+			public const int mydatareportlibrarybtn = 2131165188;
+			
+			// aapt resource value: 0x7f070003
+			public const int processnewimagebtn = 2131165187;
 			
 			static Id()
 			{
@@ -110,7 +132,13 @@ namespace BambuShootProject.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Instruction_Dialog = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int LoadImage = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Main = 2130903042;
 			
 			static Layout()
 			{
@@ -125,11 +153,11 @@ namespace BambuShootProject.Droid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
@@ -137,6 +165,22 @@ namespace BambuShootProject.Droid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int dialog_animation = 2131099648;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
