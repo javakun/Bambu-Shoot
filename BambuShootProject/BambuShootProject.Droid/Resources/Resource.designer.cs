7dffb83fa738735c35982d42316d004eee45cf8a
javier colon <javier.colon15@upr.edu>
2017-03-27 02:55:09 -0400

    no message

--- BambuShootProject/BambuShootProject.Droid/Resources/Resource.designer.cs
+++ BambuShootProject/BambuShootProject.Droid/Resources/Resource.designer.cs
@@ -100,23 +100,26 @@ namespace BambuShootProject.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070001
-			public const int BambuShootLogo = 2131165185;
+			// aapt resource value: 0x7f070002
+			public const int BambuShootLogo = 2131165186;
 			
 			// aapt resource value: 0x7f070000
 			public const int Instructions = 2131165184;
 			
-			// aapt resource value: 0x7f070005
-			public const int databasebtn = 2131165189;
+			// aapt resource value: 0x7f070006
+			public const int databasebtn = 2131165190;
 			
-			// aapt resource value: 0x7f070002
-			public const int instructionsbtn = 2131165186;
-			
-			// aapt resource value: 0x7f070004
-			public const int mydatareportlibrarybtn = 2131165188;
+			// aapt resource value: 0x7f070001
+			public const int imageView1 = 2131165185;
 			
 			// aapt resource value: 0x7f070003
-			public const int processnewimagebtn = 2131165187;
+			public const int instructionsbtn = 2131165187;
+			
+			// aapt resource value: 0x7f070005
+			public const int mydatareportlibrarybtn = 2131165189;
+			
+			// aapt resource value: 0x7f070004
+			public const int processnewimagebtn = 2131165188;
 			
 			static Id()
 			{
